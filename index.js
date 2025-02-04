c87fa33cdce4d955ceca8b3cabb1eb33bb45c19d
Hanna Turunen <hannamatilda.turunen@gmail.com>
2025-02-04 19:28:46 +0200

    Tehtävät 3.1-3.7

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ let persons = [
 ];
 
 app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+  response.send("<h1>HelloWorld!</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
