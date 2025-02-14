3dcc33647646c4d0fe4773494dcd24e119292407
Hanna Turunen <hannamatilda.turunen@gmail.com>
2025-02-14 13:58:51 +0200

    Tehtävä 3.9

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require("express");
-var morgan = require("morgan");
+const express = require("express")
+var morgan = require("morgan")
 
-const app = express();
+const app = express()
 
-app.use(morgan("tiny"));
-app.use(express.json());
+app.use(morgan("tiny"))
+app.use(express.json())
+const cors = require("cors")
+
+app.use(cors())
 
 let persons = [
   {
@@ -27,82 +30,82 @@ let persons = [
     name: "Mary Poppendieck",
     number: "39-23-6423122",
   },
-];
+]
 
 app.get("/", (request, response) => {
-  response.send("<h1>HelloWorld!</h1>");
-});
+  response.send("<h1>HelloWorld!</h1>")
+})
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
+  response.json(persons)
+})
 
-const count = persons.length;
-const time = new Date();
+const count = persons.length
+const time = new Date()
 app.get("/api/info", (request, response) => {
   response.send(
     `<p>Phonebook has info for ${count} people </p>
      <p> ${time}</p>`
-  );
-});
+  )
+})
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
+  const id = Number(request.params.id)
+  const person = persons.find((person) => person.id === id)
   if (person) {
-    response.json(person);
+    response.json(person)
   } else {
-    response.status(404).end();
+    response.status(404).end()
   }
-});
+})
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
+  const id = Number(request.params.id)
+  persons = persons.filter((person) => person.id !== id)
 
-  response.status(204).end();
-});
+  response.status(204).end()
+})
 
 const generateId = () => {
-  return Math.floor(Math.random() * 1000);
-};
+  return Math.floor(Math.random() * 1000)
+}
 
 const nameExists = (name) => {
-  const names = persons.map((person) => person.name === name);
-  return names.includes(true);
-};
+  const names = persons.map((person) => person.name === name)
+  return names.includes(true)
+}
 
 app.post("/api/persons", (request, response) => {
-  const body = request.body;
-
+  const body = request.body
+  console.log(body)
   if (!body.name) {
     return response.status(400).json({
       error: "name is missing",
-    });
+    })
   }
   if (!body.number) {
     return response.status(400).json({
       error: "number is missing",
-    });
+    })
   }
   if (nameExists(body.name)) {
     return response.status(400).json({
       error: "name must be unique",
-    });
+    })
   }
 
   const person = {
     id: generateId(),
     name: body.name,
     number: body.number,
-  };
+  }
 
-  persons = persons.concat(person);
+  persons = persons.concat(person)
 
-  response.json(person);
-});
+  response.json(person)
+})
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
