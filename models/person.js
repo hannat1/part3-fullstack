5341d76821af48bdbf7a25148088f7a2818dfae6
Hanna Turunen <hannamatilda.turunen@gmail.com>
2026-05-25 13:58:11 +0300

    3.19-3.21

--- models/person.js
+++ models/person.js
@@ -16,8 +16,22 @@ mongoose
   })
 
 const phonebookSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /^(\d{3}-\d{5,}|\d{2}-\d{6,})/.test(v)
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 })
 
 phonebookSchema.set("toJSON", {
